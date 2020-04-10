3847dac1b1607d357528b9132963252cc723b65c
“Evan <eli24@ucsc.edu>
2020-04-10 15:51:03 -0700

    Added rocket and spaceships

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,35 @@
+// Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);   // add object to existing scene, displayList, updateList
+        this.isFiring = false;  // track rocket's firing status
+    }
+
+    update() {
+        // left, right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= 47) {
+                this.x-= 3;
+            } else if(keyRIGHT.isDown && this.x <= 577) {
+                this.x += 3;
+            }
+        }
+
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.isFiring = true;
+        }
+
+        if(this.isFiring && this.y >= 108) {
+            this.y -= 6;
+        }
+
+        // reset on miss 
+        if(this.y <= 108) {
+            this.isFiring = false;
+            this.y = 431;
+        }
+    }
+}
